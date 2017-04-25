21bee0c5ad475a7c3625b0df2ee49dc87feb16b4
destruktoid <destruktoid@gmail.com>
2017-04-25 23:58:44 +1000

    Macros, fixed LMGs and fucked off Rakurs thing

---
+++ addons/DEST_menu_sor/functions/sel_macros.hpp
@@ -0,0 +1,72 @@
+//macro name format: regiment_name_variant_type
+
+#define INF_STD_D_W ["HK416 Black","160_weap_inf_std"],\
+            ["HK416 Desert","arifle_SPAR_01_snd_F"],\
+            ["M16A4 Black","rhs_weap_m16a4"],\
+            ["M4A1 Black","rhs_weap_m4"],\
+            ["M4A1 Desert","rhs_weap_m4a1_blockII_d"],\
+            ["MK18 Desert","rhs_weap_mk18_d"],\
+            ["MK18 Black","rhs_weap_mk18_KAC_bk"],\
+            ["M27 IAR","rhs_weap_m27iar"]
+            
+#define INF_STD_W_W ["HK416 Black","160_weap_inf_std"],\
+            ["HK416 Woodland","arifle_SPAR_01_khk_F"],\
+            ["M16A4 Black","rhs_weap_m16a4"],\
+            ["M4A1 Black","rhs_weap_m4"],\
+            ["M4A1 Woodland","rhs_weap_m4a1_blockII_wd"],\
+            ["MK18 Black","rhs_weap_mk18_KAC_bk"],\
+            ["MK18 Woodland","rhs_weap_mk18_wd"],\
+            ["M27 IAR","rhs_weap_m27iar"]
+            
+#define INF_STD_D_O ["HOLO Desert","optic_Holosight"],\
+            ["HOLO Black","optic_Holosight_blk_F"],\
+            ["RCO","optic_Hamr"],\
+            ["MRCO","optic_MRCO"],\
+            ["CCO","rhsusf_acc_compm4"],\
+            ["ACO Red","optic_Aco"],\
+            ["ACO Green","optic_ACO_grn"],\
+            ["ERCO Black","optic_ERCO_blk_F"],\
+            ["ERCO Desert","optic_ERCO_snd_F"],\
+            ["ARCO Black","optic_Arco_blk_F"],\
+            ["ACOG","rhsusf_acc_ACOG_USMC"],\
+            ["ACOG ACO Black","rhsusf_acc_ACOG_RMR"],\
+            ["ACOG Desert","rhsusf_acc_ACOG_d"]
+            
+#define INF_STD_W_O ["HOLO Black","optic_Holosight_blk_F"],\
+            ["RCO","optic_Hamr"],\
+            ["MRCO","optic_MRCO"],\
+            ["CCO","rhsusf_acc_compm4"],\
+            ["ACO Red","optic_Aco"],\
+            ["ACO Green","optic_ACO_grn"],\
+            ["ERCO Black","optic_ERCO_blk_F"],\
+            ["ERCO Woodland","optic_ERCO_khk_F"],\
+            ["ARCO Black","optic_Arco_blk_F"],\
+            ["ACOG","rhsusf_acc_ACOG_USMC"],\
+            ["ACOG ACO Black","rhsusf_acc_ACOG_RMR"],\
+            ["ACOG Woodland","rhsusf_acc_ACOG_wd"]
+            
+#define INF_GND_D_W ["HK416 Black","160_weap_inf_gl"],\
+            ["HK416 Desert","arifle_SPAR_01_GL_snd_F"],\
+            ["M16A4 Black","rhs_weap_m16a4_carryhandle_M203"],\
+            ["M4 M320 Black","rhs_weap_m4_m320"],\
+            ["M4A1 M203 Black","rhs_weap_m4a1_carryhandle_m203S"],\
+            ["M4A1 M203 Desert","rhs_weap_m4a1_blockII_M203_d"],\
+            ["MK18 Black","rhs_weap_mk18_m320"]
+            
+#define INF_GND_W_W ["HK416 Black","160_weap_inf_gl"],\
+            ["HK416 Woodland","arifle_SPAR_01_GL_khk_F"],\
+            ["M16A4 Black","rhs_weap_m16a4_carryhandle_M203"],\
+            ["M4 M320 Black","rhs_weap_m4_m320"],\
+            ["M4A1 M203 Black","rhs_weap_m4a1_carryhandle_m203S"],\
+            ["M4A1 M203 Woodland","rhs_weap_m4a1_blockII_M203_wd"],\
+            ["MK18 Black","rhs_weap_mk18_m320"]
+            
+#define INF_249_W ["M249 SPW","160_weap_inf_m249"]
+
+#define INF_240_W ["M240B","160_weap_inf_m240"]
+
+#define INF_AR_O ["HOLO Sand","optic_Holosight"],\
+            ["CCO","rhsusf_acc_compm4"],\
+            ["ACO Red","optic_Aco"],\
+            ["ACO Green","optic_ACO_grn"],\
+            ["HOLO Black","optic_Holosight_blk_F"]
