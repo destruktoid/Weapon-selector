e024cc2895aeabaac93dcfbc5661109e686b3e14
yettie79 <lenny.henderson@bigpond.com>
2017-08-13 15:41:18 +1000

    SCAR AND ACR ADDED, SOME SCOPES ADDED

--- addons/160_Selector/functions/sel_macros.hpp
+++ addons/160_Selector/functions/sel_macros.hpp
@@ -3,6 +3,10 @@
 #define INF_STD_D_W ["HK416 Black","160_weap_inf_std"],\
             ["M4 Black","SMA_M4afg"],\
             ["M4 Desert","SMA_M4afg_Tan"],\
+			["SCAR L Desert","SMA_MK16"],\
+			["SCAR L Black","SMA_MK16_black"],\
+			["ACR Black","SMA_ACRREMMOEblk_N"],\
+            ["ACR Desert","SMA_ACRREMMOE_N"],\
             ["MK18 Desert","SMA_MK18afgTAN"],\
             ["MK18 Black","SMA_MK18afg"]
             
@@ -10,16 +14,19 @@
             ["HK416 Woodland","SMA_HK416afgOD"],\
             ["M4A1 Black","SMA_M4afg"],\
             ["M4A1 Woodland","SMA_M4afg_OD"],\
+			["SCAR L Black","SMA_MK16_black"],\
+			["ACR Black","SMA_ACRREMMOEblk_N"],\
             ["MK18 Black","SMA_MK18afg"],\
             ["MK18 Woodland","SMA_MK18afgOD"]
             
-#define INF_STD_D_O ["HOLO Desert","optic_Holosight"],\
+#define INF_STD_D_O ["HOLO Desert","rhsusf_acc_eotech_552_d"],\
             ["HOLO Black","optic_Holosight_blk_F"],\
             ["RCO","optic_Hamr"],\
             ["MRCO","optic_MRCO"],\
             ["CCO","rhsusf_acc_compm4"],\
             ["ACO Red","optic_Aco"],\
-            ["ACO Green","optic_ACO_grn"],\
+            ["RDS Black","SMA_BARSKA"],\
+			["ACO Woodland","SMA_CMOREGRN"],\
             ["ERCO Black","optic_ERCO_blk_F"],\
             ["ERCO Desert","optic_ERCO_snd_F"],\
             ["ARCO Black","optic_Arco_blk_F"],\
@@ -28,11 +35,13 @@
             ["ACOG Desert","rhsusf_acc_ACOG_d"]
             
 #define INF_STD_W_O ["HOLO Black","optic_Holosight_blk_F"],\
+			["HOLO Woorland","SMA_eotech_G"],\
             ["RCO","optic_Hamr"],\
             ["MRCO","optic_MRCO"],\
             ["CCO","rhsusf_acc_compm4"],\
             ["ACO Red","optic_Aco"],\
-            ["ACO Green","optic_ACO_grn"],\
+            ["RDS Black","SMA_BARSKA"],\
+			["ACO Woodland","SMA_CMOREGRN"],\
             ["ERCO Black","optic_ERCO_blk_F"],\
             ["ERCO Woodland","optic_ERCO_khk_F"],\
             ["ARCO Black","optic_Arco_blk_F"],\
@@ -42,22 +51,35 @@
             
 #define INF_GND_D_W ["HK416 Black","160_weap_inf_gl"],\
             ["M4 M320 Black","SMA_M4_GL"],\
+			["SCAR L Desert","SMA_Mk16_EGLM"],\
+			["SCAR L Black","SMA_Mk16_EGLM_black"],\
+			["ACR Black","SMA_ACRREMGL_B_N"],\
+			["ACR Desert","SMA_ACRREMGL_N"],\
             ["MK18 Black","SMA_MK18_GL"],\
             ["MK18 Desert","SMA_MK18TAN_GL"]
             
 #define INF_GND_W_W ["HK416 Black","160_weap_inf_gl"],\
             ["HK416 Woodland","SMA_HK416GLOD"],\
             ["M4 Black","SMA_M4_GL"],\
+			["SCAR L Black","SMA_Mk16_EGLM_black"],\
+			["ACR Black","SMA_ACRREMGL_B_N"],\
             ["MK18 Woodland","SMA_MK18OD_GL"],\
             ["MK18 Black","SMA_MK18BLK_GL"]
             
-#define INF_249_W ["M249 SPW","160_weap_inf_AR1"]
-
-#define INF_240_W ["M240B","160_weap_inf_AR2"]
+#define INF_249_W ["Maximi Long Black","sma_minimi_mk3_762tlb"],\
+			["Maximi Short Black","sma_minimi_mk3_762tsb"],\
+			["Maximi Long Desert","sma_minimi_mk3_762tlb_des"],\
+			["Maximi Short Desert","sma_minimi_mk3_762tsb_des"]
+			
+#define INF_240_W ["Maximi Long Black","sma_minimi_mk3_762tlb"],\
+			["Maximi Short Black","sma_minimi_mk3_762tsb"],\
+			["Maximi Long Woodland","sma_minimi_mk3_762tlb_wdl"],\
+			["Maximi Short Woodland","sma_minimi_mk3_762tsb_wdl"]
 
 #define INF_AR_O ["HOLO Sand","optic_Holosight"],\
             ["CCO","rhsusf_acc_compm4"],\
             ["ACO Red","optic_Aco"],\
             ["ACO Green","optic_ACO_grn"],\
             ["HOLO Black","optic_Holosight_blk_F"],\
+			["ARCO Desert","optic_Arco"],\
             ["ARCO Black","optic_Arco_blk_F"]
